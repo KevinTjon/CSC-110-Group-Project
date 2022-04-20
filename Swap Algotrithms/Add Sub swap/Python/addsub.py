27141c76691db5998dd189ee61c15d5caaed1d90
KevinTjon <100467756+KevinTjon@users.noreply.github.com>
2022-04-20 09:06:48 -0400

    Create addsub.py

---
+++ Swap Algotrithms/Add Sub swap/Python/addsub.py
@@ -0,0 +1,15 @@
+
+# Python program to demonstrate
+# swapping of two variables
+ 
+x = 10
+y = 50
+ 
+# Swapping of two variables
+# using arithmetic operations
+x = x + y  
+y = x - y 
+x = x - y
+ 
+print("Value of x:", x)
+print("Value of y:", y)
