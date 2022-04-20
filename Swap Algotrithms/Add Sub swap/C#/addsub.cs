4601e4e18492128d9d93d0186cf7e42b0c250c78
KevinTjon <100467756+KevinTjon@users.noreply.github.com>
2022-04-20 10:44:00 -0400

    Create addsub.cs

---
+++ Swap Algotrithms/Add Sub swap/C#/addsub.cs
@@ -0,0 +1,18 @@
+using System;
+
+public class HelloWorld
+{
+    public static void Main(string[] args)
+    {
+    
+        int x = 1;
+        int y = 2;
+    
+       x = x + y;
+       y = x - y;
+       x = x - y;
+    
+        Console.WriteLine("x=" + " " + x);
+        Console.WriteLine("y=" + " " + y);
+    }
+}
