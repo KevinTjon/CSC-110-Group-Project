01287c4eb39e7a52777a050594202a487731cb65
KevinTjon <100467756+KevinTjon@users.noreply.github.com>
2022-04-20 09:02:48 -0400

    Create parallel.py

---
+++ Swap Algotrithms/Parallel Assignment/Python/parallel.py
@@ -0,0 +1,12 @@
+# Python program to demonstrate
+# swapping of two variables
+ 
+x = 10
+y = 50
+ 
+# Swapping of two variables
+# without using third variable
+x, y = y, x
+ 
+print("Value of x:", x)
+print("Value of y:", y)
