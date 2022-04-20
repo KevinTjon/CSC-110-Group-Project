bc49c0407666706ad52f0d2cc62de8297fc08812
KevinTjon <100467756+KevinTjon@users.noreply.github.com>
2022-04-20 10:49:47 -0400

    Create xor.rs

---
+++ Swap Algotrithms/XOR swap/Rust/xor.rs
@@ -0,0 +1,19 @@
+// Rust program to swap two numbers 
+// using bitwise XOR (^) operator
+
+fn main() {
+    let mut num1:i32 = 6;
+    let mut num2:i32 = 2;
+    
+    println!("Numbers before swapping:");
+    println!("\tNum1: {}",num1);
+    println!("\tNum2: {}",num2);
+
+    num1 = num1 ^ num2;
+    num2 = num1 ^ num2;
+    num1 = num1 ^ num2;
+
+    println!("Numbers after swapping:");
+    println!("\tNum1: {}",num1);
+    println!("\tNum2: {}",num2);
+}
